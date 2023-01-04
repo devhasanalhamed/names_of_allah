1a4c4d2aeea1fd6d73e28db0c54d17cd05f425e9
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-01-05 02:02:19 +0300

    brand new structure
    the new and my favorite theme to now

--- lib/view/widgets/list_of_names.dart
+++ lib/view/widgets/list_of_names.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:names_of_allah/models/names.dart';
 
-class ListOfNames extends StatelessWidget {
+class ListOfNames extends StatefulWidget {
   const ListOfNames(this.names, this.function, {Key? key}) : super(key: key);
 
   final Function function;
   final List<Names> names;
 
+  @override
+  State<ListOfNames> createState() => _ListOfNamesState();
+}
+
+class _ListOfNamesState extends State<ListOfNames> {
+  var clicked = false;
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -18,60 +24,121 @@ class ListOfNames extends StatelessWidget {
               // topLeft: Radius.circular(15),
               ),
         ),
-        margin: const EdgeInsets.symmetric(horizontal: 20),
+        margin: const EdgeInsets.symmetric(horizontal: 0),
         padding: const EdgeInsets.only(top: 0.0),
-        child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisExtent: 80,
-            ),
-            itemCount: names.length,
+        child: ListView.builder(
+            itemCount: widget.names.length,
             itemBuilder: (ctx, index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Theme.of(context)
-                          .colorScheme
-                          .secondary
-                          .withOpacity(0.5),
+              return NamesHolder(
+                  widget.names[index].name, widget.names[index].meaning);
+            }),
+      ),
+    );
+  }
+}
+
+class NamesHolder extends StatefulWidget {
+  final String name;
+  final String meaning;
+  const NamesHolder(this.name, this.meaning, {Key? key}) : super(key: key);
+
+  @override
+  State<NamesHolder> createState() => _NamesHolder();
+}
+
+class _NamesHolder extends State<NamesHolder> {
+  var clicked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 10,
+      ),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: clicked ? 500 : 100),
+        width: double.infinity,
+        height: clicked ? 250 : 60,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(12),
+          ),
+          color: Theme.of(context).primaryColor,
+        ),
+        child: InkWell(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          onTap: () {
+            setState(() {
+              clicked = !clicked;
+            });
+          },
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Column(
+                children: [
+                  Text(
+                    widget.name,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
                     ),
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(12),
-                    ),
-                    color: Theme.of(context).primaryColor,
                   ),
-                  child: InkWell(
-                    borderRadius: const BorderRadius.all(Radius.circular(12)),
-                    onTap: () =>
-                        function(names[index].name, names[index].meaning),
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: Stack(
-                          alignment: Alignment.center,
+                  if (clicked)
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.all(15),
+                        margin: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(12),
+                          ),
+                          color: Theme.of(context)
+                              .colorScheme
+                              .background
+                              .withOpacity(0.5),
+                        ),
+                        child: ListView(
                           children: [
-                            // Image.asset('assets/images/name_holder.png'),
                             Text(
-                              names[index].name.toString(),
-                              style: TextStyle(
-                                fontSize: 24,
-                                color: Colors.black,
+                              widget.meaning,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
                               ),
                             ),
                           ],
                         ),
                       ),
                     ),
+                  AnimatedContainer(
+                    duration: Duration(milliseconds: clicked ? 300 : 100),
+                    height: clicked ? 50 : 0,
+                    child: clicked
+                        ? Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: const [
+                              Icon(
+                                Icons.share,
+                              ),
+                              Icon(
+                                Icons.favorite,
+                              ),
+                              Icon(
+                                Icons.copy,
+                              ),
+                            ],
+                          )
+                        : null,
                   ),
-                ),
-              );
-            }),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
