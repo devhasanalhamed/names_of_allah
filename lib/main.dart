994379981b7428ac635a7003091c681da2532c9f
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-01-09 05:08:57 +0300

    applying the dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:names_of_allah/app/providers/namesProvider.dart';
-import 'package:names_of_allah/core/colors/theme_constants.dart';
 import 'package:names_of_allah/core/controllers/theme_manager.dart';
 import 'package:names_of_allah/core/data/light_theme_data.dart';
 import 'app/view/screens/home_page.dart';
 import 'package:provider/provider.dart';
+
+import 'core/data/dark_theme_data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         builder: (context, theme, child) => MaterialApp(
           title: 'أسماء الله الحُسنى',
           theme: getLightThemeData(),
-          darkTheme: darkTheme,
+          darkTheme: getDarkThemeData(),
           themeMode: Provider.of<ThemeManager>(context).themeMode,
           home: ChangeNotifierProvider(
             create: (ctx) => DataProvider(),
