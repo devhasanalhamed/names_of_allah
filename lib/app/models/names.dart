2b6f0a02e8c9fd860c17f492347880934a89557b
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-01-14 07:28:41 +0300

    Update names.dart

--- lib/app/models/names.dart
+++ lib/app/models/names.dart
@@ -2,10 +2,12 @@ class Names {
   String name;
   String meaning;
   String search;
+  bool favorite;
 
   Names({
     required this.name,
     required this.meaning,
     required this.search,
+    required this.favorite,
   });
 }
