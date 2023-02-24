df14c040604136d973835171077a99ddb360c5eb
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-02-24 20:10:12 +0300

    add id to names model

--- lib/app/models/names.dart
+++ lib/app/models/names.dart
@@ -1,4 +1,5 @@
 class Names {
+  int id;
   String name;
   String meaning;
   String search;
@@ -6,6 +7,7 @@ class Names {
   bool open;
 
   Names({
+    required this.id,
     required this.name,
     required this.meaning,
     required this.search,
