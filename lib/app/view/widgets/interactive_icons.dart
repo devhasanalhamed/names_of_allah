06f821fd211e26f4c8bbdcbfa6a7391a31597aca
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-06 09:59:53 +0300

    Update interactive_icons.dart

--- lib/app/view/widgets/interactive_icons.dart
+++ lib/app/view/widgets/interactive_icons.dart
@@ -4,15 +4,13 @@ import 'package:provider/provider.dart';
 
 class InteractiveIcons extends StatelessWidget {
   const InteractiveIcons({
-    Key? key,
-    required this.clicked,
+    super.key,
     required this.name,
     required this.meaning,
     required this.favorite,
     required this.shareIt,
-  }) : super(key: key);
+  });
 
-  final bool clicked;
   final String name;
   final String meaning;
   final bool favorite;
@@ -20,42 +18,38 @@ class InteractiveIcons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: Duration(milliseconds: clicked ? 300 : 100),
-      height: clicked ? 50 : 0,
-      child: clicked
-          ? Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  onPressed: shareIt,
-                  icon: const Icon(Icons.share),
-                ),
-                IconButton(
-                  onPressed: () {
-                    Provider.of<DataProvider>(context, listen: false)
-                        .addFavorite(name);
-                    Provider.of<DataProvider>(context, listen: false)
-                        .showSnakBar(
-                      context,
-                      favorite ? 'إزالة من المفضلة' : 'إضافة إلى المفضلة',
-                    );
-                  },
-                  icon: favorite
-                      ? const Icon(Icons.favorite)
-                      : const Icon(Icons.favorite_border),
-                  color: Colors.red,
-                ),
-                IconButton(
-                  onPressed: () {
-                    Provider.of<DataProvider>(context, listen: false)
-                        .copyToClipboard(context, name, meaning);
-                  },
-                  icon: const Icon(Icons.copy),
-                ),
-              ],
-            )
-          : null,
+    return SizedBox(
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            onPressed: shareIt,
+            icon: const Icon(Icons.share),
+          ),
+          IconButton(
+            onPressed: () {
+              Provider.of<DataProvider>(context, listen: false)
+                  .addFavorite(name);
+              Provider.of<DataProvider>(context, listen: false).showSnackBar(
+                context,
+                favorite ? 'إزالة من المفضلة' : 'إضافة إلى المفضلة',
+              );
+            },
+            icon: favorite
+                ? const Icon(Icons.favorite)
+                : const Icon(Icons.favorite_border),
+            color: Colors.red,
+          ),
+          IconButton(
+            onPressed: () {
+              Provider.of<DataProvider>(context, listen: false)
+                  .copyToClipboard(context, name, meaning);
+            },
+            icon: const Icon(Icons.copy),
+          ),
+        ],
+      ),
     );
   }
 }
