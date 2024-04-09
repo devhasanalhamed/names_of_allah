ea2aa6e10acb2fced753811c2e9152b5fa4e9a4c
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-09 22:33:54 +0300

    Update names.dart

--- lib/app/models/names.dart
+++ lib/app/models/names.dart
@@ -4,7 +4,6 @@ class Names {
   String meaning;
   String search;
   bool favorite;
-  bool open;
 
   Names({
     required this.id,
@@ -12,7 +11,6 @@ class Names {
     required this.meaning,
     required this.search,
     required this.favorite,
-    required this.open,
   });
 }
 //for view only
