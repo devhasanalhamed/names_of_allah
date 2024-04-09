ce11101e95d07b066dfee271ea90d4da7c803b07
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-09 22:34:07 +0300

    Update theme_manager.dart

--- lib/core/controllers/theme_manager.dart
+++ lib/core/controllers/theme_manager.dart
@@ -5,22 +5,22 @@ import 'package:shared_preferences/shared_preferences.dart';
 class ThemeManager with ChangeNotifier {
   bool _isDarkMode = false;
 
-  get themeMode => _isDarkMode?ThemeMode.dark:ThemeMode.light;
+  get themeMode => _isDarkMode ? ThemeMode.dark : ThemeMode.light;
   get isDarkMode => _isDarkMode;
 
-  Future<void> getPrefs() async {
-    final prefs = await SharedPreferences.getInstance();
-    final theme = prefs.getBool('theme');
-    if(theme != null){
+  Future<void> getPref() async {
+    final pref = await SharedPreferences.getInstance();
+    final theme = pref.getBool('theme');
+    if (theme != null) {
       _isDarkMode = theme;
     }
     notifyListeners();
   }
 
   Future<void> toggleTheme() async {
-    final prefs = await SharedPreferences.getInstance();
+    final pref = await SharedPreferences.getInstance();
     _isDarkMode = !_isDarkMode;
-    await prefs.setBool('theme', _isDarkMode);
+    await pref.setBool('theme', _isDarkMode);
     notifyListeners();
   }
 }
