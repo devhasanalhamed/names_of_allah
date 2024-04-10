60b72ebd33b97e388b05ebcd096912837db004ef
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-10 06:43:40 +0300

    Create name_model.dart

---
+++ lib/app/data/models/name_model.dart
@@ -0,0 +1,15 @@
+class NameModel {
+  int id;
+  String name;
+  String meaning;
+  String search;
+  bool favorite;
+
+  NameModel({
+    required this.id,
+    required this.name,
+    required this.meaning,
+    required this.search,
+    required this.favorite,
+  });
+}
