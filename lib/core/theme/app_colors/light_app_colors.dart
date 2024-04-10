f7f16bca41cc9757dc53b7a8844978cd92dad899
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-10 06:44:30 +0300

    Create light_app_colors.dart

---
+++ lib/core/theme/app_colors/light_app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ColorsLight {
+  static const primaryColor = Color(0xFF192837);
+  static const secondaryColor = Color(0xFFFFFFFF);
+  static const backgroundColor = Color(0xFFF1F1F1);
+  static const textColor = Colors.black;
+
+  static const textColorBodyLarge = primaryColor;
+  static const textColorBodyMedium = textColor;
+  static const textColorBodySmall = primaryColor;
+
+  static const textColorTitleLarge = primaryColor;
+  static const textColorTitleMedium = primaryColor;
+  static const textColorTitleSmall = primaryColor;
+
+  static const iconColor = primaryColor;
+  static const splashColor = primaryColor;
+}
